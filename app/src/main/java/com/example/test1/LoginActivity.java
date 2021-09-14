f2ccb772e16aa66035268aac8f4c2c637b1a9a8a
BUICONGTHANG <thangbcph11219@fpt.edu.vn>
2021-09-14 21:29:57 +0700

    duantotnghiep

---
+++ app/src/main/java/com/example/test1/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    LinearLayout lnrLoginGG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        lnrLoginGG = findViewById(R.id.lnrLoginGG);
+        lnrLoginGG.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+            }
+        });
+
+    }
+}
