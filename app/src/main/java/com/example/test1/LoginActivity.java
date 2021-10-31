6ff262b1789ef747f0f2593757300ebcf5424352
docaothang24 <docaothang2402@gmail.com>
2021-10-31 22:42:11 +0700

    add API searchUser

--- app/src/main/java/com/example/test1/LoginActivity.java
+++ app/src/main/java/com/example/test1/LoginActivity.java
@@ -3,14 +3,24 @@ package com.example.test1;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.example.test1.fragments.HomeFragment;
+import com.example.test1.models.Users;
+import com.example.test1.volleys.FunctionUserVolley;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -23,11 +33,19 @@ import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
 
     LinearLayout lnrLoginGG;
     GoogleSignInClient mGoogleSignInClient;
     private static int RC_SIGN_IN = 100;
+    public static boolean moveActivity = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,13 +99,25 @@ public class LoginActivity extends AppCompatActivity {
                 Uri personPhoto = acct.getPhotoUrl();
 
                 int index = personEmail.indexOf("@");
-                if(personEmail.substring(index,personEmail.length()).equals("@fpt.edu.vn")){
-                    Intent intent = new Intent(LoginActivity.this,NameActivity.class);
-                    intent.putExtra("email",personEmail);
-                    startActivity(intent);
-                    Toast.makeText(this, "Thành công", Toast.LENGTH_SHORT).show();
-                    Log.e("mail",personEmail);
-                }else {
+                if (personEmail.substring(index, personEmail.length()).equals("@fpt.edu.vn")) {
+                    Log.e("login",personEmail);
+                    FunctionUserVolley functionUserVolley = new FunctionUserVolley();
+                    functionUserVolley.searchUser_GET(personEmail);
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (moveActivity == true) {
+                                Intent intent = new Intent(LoginActivity.this, NameActivity.class);
+                                intent.putExtra("email", personEmail);
+                                startActivity(intent);
+                            } else {
+                                Intent intent1 = new Intent(LoginActivity.this, HomeFragment.class);
+                                startActivity(intent1);
+                            }
+                        }
+                    }, 2000);
+                } else {
                     Toast.makeText(this, "Thất bại", Toast.LENGTH_SHORT).show();
                     signOut();
                 }
--- app/src/main/java/com/example/test1/volleys/FunctionUserVolley.java
+++ app/src/main/java/com/example/test1/volleys/FunctionUserVolley.java
@@ -1,6 +1,7 @@
 package com.example.test1.volleys;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -18,8 +19,13 @@ import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
+import com.example.test1.LoginActivity;
+import com.example.test1.NameActivity;
+import com.example.test1.fragments.HomeFragment;
 import com.example.test1.models.InfoRegister;
+import com.example.test1.models.Users;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -53,21 +59,21 @@ public class FunctionUserVolley {
 //        } catch (JSONException e) {
 //            e.printStackTrace();
 //        }
-        Log.e("xyz",infoRegister.getEmail()+"\n"+infoRegister.getName()+"\n"+infoRegister.getBirthday()
-                +"\n"+infoRegister.getSex()+"\n"+infoRegister.getSpecialized()+"\n"+infoRegister.getCourse()+"\n"+infoRegister.getAddressStudy()
-                +"\n"+infoRegister.getInterests()+"\n"+infoRegister.getImages()+"\n"+infoRegister.getShow());
+        Log.e("xyz", infoRegister.getEmail() + "\n" + infoRegister.getName() + "\n" + infoRegister.getBirthday()
+                + "\n" + infoRegister.getSex() + "\n" + infoRegister.getSpecialized() + "\n" + infoRegister.getCourse() + "\n" + infoRegister.getAddressStudy()
+                + "\n" + infoRegister.getInterests() + "\n" + infoRegister.getImages() + "\n" + infoRegister.getShow());
 
         AndroidNetworking.upload("https://poly-dating.herokuapp.com/api/users/insert")
-                .addMultipartParameter("email","duyenrach@gmail.com")
-                .addMultipartParameter("name",infoRegister.getName())
-                .addMultipartFileList("avatars",infoRegister.getImages())
+                .addMultipartParameter("email", "duyenrach@gmail.com")
+                .addMultipartParameter("name", infoRegister.getName())
+                .addMultipartFileList("avatars", infoRegister.getImages())
                 .addMultipartParameter("hobbies", Arrays.toString(infoRegister.getInterests()))
-                .addMultipartParameter("isShow",infoRegister.getShow())
-                .addMultipartParameter("birthDay",infoRegister.getBirthday())
-                .addMultipartParameter("gender",infoRegister.getSex())
-                .addMultipartParameter("facilities",infoRegister.getAddressStudy())
-                .addMultipartParameter("specialized",infoRegister.getSpecialized())
-                .addMultipartParameter("course",infoRegister.getCourse())
+                .addMultipartParameter("isShow", infoRegister.getShow())
+                .addMultipartParameter("birthDay", infoRegister.getBirthday())
+                .addMultipartParameter("gender", infoRegister.getSex())
+                .addMultipartParameter("facilities", infoRegister.getAddressStudy())
+                .addMultipartParameter("specialized", infoRegister.getSpecialized())
+                .addMultipartParameter("course", infoRegister.getCourse())
                 .setPriority(Priority.MEDIUM)
                 .build()
                 .getAsJSONObject(new JSONObjectRequestListener() {
@@ -138,6 +144,76 @@ public class FunctionUserVolley {
 //        };
         // đưa vào request
 //        queue.add(stringRequest);
+    }
+
+    public void searchUser_GET(String email) {
+        LoginActivity loginActivity = new LoginActivity();
+        String url = "https://poly-dating.herokuapp.com/api/users/search/{email}";
+        AndroidNetworking.get(url)
+                .addPathParameter("email", email)
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+//
+                        try {
+                            if (response.getString("data").equals("null")) {
+                                loginActivity.moveActivity = true;
+                            } else {
+                                List<String> fileimg = new ArrayList<>();
+                                JSONObject jo = response.getJSONObject("data").getJSONObject("user");
+                                Log.e("data:", response.toString());
+                                String email = jo.getString("email");
+                                String name = jo.getString("name");
+                                JSONArray avatars = jo.getJSONArray("avatars");
+                                for (int j = 0; j < avatars.length(); j++) {
+                                    fileimg.add(avatars.getString(j));
+                                }
+                                String hobbies = jo.getString("hobbies");
+                                String birthDay = jo.getString("birthDay");
+                                String gender = jo.getString("gender");
+                                String description = jo.getString("description");
+                                String facilities = jo.getString("facilities");
+                                String specialized = jo.getString("specialized");
+                                String course = jo.getString("course");
+                                String isShow = jo.getString("isShow");
+                                String isActive = jo.getString("isActive");
+                                String status = jo.getString("status");
+
+                                Users users = new Users();
+                                users.setEmail(email);
+                                users.setName(name);
+                                users.setAvatars(fileimg);
+                                users.setHobbies(hobbies);
+                                users.setBirthDay(birthDay);
+                                users.setGender(gender);
+                                users.setDescription(description);
+                                users.setFacilities(facilities);
+                                users.setSpecialized(specialized);
+                                users.setCourse(course);
+                                users.setIsShow(isShow);
+                                users.setIsActive(isActive);
+                                users.setStatus(status);
+
+                                if (users.getIsActive().equals("Kích hoạt")) {
+                                    loginActivity.moveActivity = false;
+                                } else {
+                                    Log.e("Not Active", users.getIsActive());
+
+                                }
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.e("err:", anError.getResponse().toString());
+                    }
+                });
     }
 
 }
