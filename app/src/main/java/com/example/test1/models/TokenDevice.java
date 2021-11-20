b4b2c509624d3e8b3b4c1397d5b62ca308292d63
docaothang24 <docaothang2402@gmail.com>
2021-11-20 22:27:30 +0700

    notification firebase

---
+++ app/src/main/java/com/example/test1/models/TokenDevice.java
@@ -0,0 +1,20 @@
+package com.example.test1.models;
+
+public class TokenDevice {
+    String token;
+
+    public TokenDevice(String token) {
+        this.token = token;
+    }
+
+    public TokenDevice() {
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
